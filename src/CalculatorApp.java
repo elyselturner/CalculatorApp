70aa9ecac6d74f3b76cf48757802a945dda6e576
elyselturner <elyse.turner@detroitlabs.com>
2014-09-14 14:25:41 -0400

    Added feature that accepts users #'s

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -6,10 +6,24 @@ package com.example.CalculatorApp;
 //This calculator app will take user input, remember it, select a mathematical operator and display the answer
 //after the answer is displayed, it will need to ask if the user would like to calculate anything else
 
-import java.util.Scanner;
-public class CalculatorApp {
-    public static void main(String [] args){
-        Scanner UserInput = new Scanner(System.in);
+//First, I imported a scanner so I could remember my users input. I ask the user for their first number, int num1,
+//and then their second, int num2.
+//
+import java.util.*;
+public class CalculatorApp{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello! Please enter your first number:");
+        Scanner inp1= new Scanner(System.in);
+        int num1;
+        num1 = inp1.nextInt();
+        int ans1;
+        System.out.println("Enter second number:");
+        Scanner inp2= new Scanner(System.in);
+        int num2;
+        num2 = inp2.nextInt();
+        int ans2;
 
-    }
-}
+
+        }
+        }

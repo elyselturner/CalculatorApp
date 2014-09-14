cd9d6bb9ee1a0ea5bdb6c6fcb9846192fa711775
elyselturner <elyse.turner@detroitlabs.com>
2014-09-14 12:54:30 -0400

    Initial Calculator setup

---
+++ src/CalculatorApp.java
@@ -0,0 +1,15 @@
+package com.example.CalculatorApp;
+
+/**
+ * Created by elyseturner on 9/14/14.
+ */
+//This calculator app will take user input, remember it, select a mathematical operator and display the answer
+//after the answer is displayed, it will need to ask if the user would like to calculate anything else
+
+import java.util.Scanner;
+public class CalculatorApp {
+    public static void main(String [] args){
+        Scanner UserInput = new Scanner(System.in);
+
+    }
+}
